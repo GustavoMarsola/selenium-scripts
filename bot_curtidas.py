235db96220a2a56f0837338088343c9391969232
Gustavo Marsola <86449687+GustavoMarsola@users.noreply.github.com>
2022-10-22 22:30:22 -0300

    Update bot_curtidas.py

--- bot_curtidas.py
+++ bot_curtidas.py
@@ -6,13 +6,12 @@
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
 import dotenv
 import os
 
-class Bot():
+class BotInstagram():
 
     def __init__(self):
         print("Carregando variáveis de ambiente do programa")
@@ -40,13 +39,22 @@ class Bot():
         password.send_keys(senha)
         entrar = self.driver.find_element(by=By.XPATH,value = '//*[@id="loginForm"]/div/div[3]/button/div')
         entrar.click()
-        sleep(5)
+        sleep(10)
     
     def carregar_pagina(self,pagina_escolhida):
-        self.driver.get(f"https://www.instagram.com/{pagina_escolhida}/?next=%2F")
+        self.driver.get(f"https://www.instagram.com/{pagina_escolhida}/")
+        sleep(5)
+        pics = []
+        links = self.driver.find_elements(by=By.TAG_NAME,value='a')
+        for link in links:
+            url = link.get_attribute("href")
+            if url.startswith("https://www.instagram.com/p/"):
+                print(url)
+                pics.append(url)
+        return pics
 
 
 if __name__ == '__main__':
-    robo = Bot()
+    robo = BotInstagram()
     insta = robo.login_instagram(os.getenv('LOGIN'),os.getenv('PASSWORD'))
     karina = robo.carregar_pagina('kasantiago.m')
